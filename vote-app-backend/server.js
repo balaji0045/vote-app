e8044e14f687ab30d36cfdb8960736d5992402f3
balaji0045 <balajidk100@gmail.com>
2025-02-04 21:51:47 +0530

    some correctios

--- vote-app-backend/server.js
+++ vote-app-backend/server.js
@@ -31,7 +31,7 @@ app.use(cors());
 
 
 const corsOptions = {
-  origin: ["http://localhost:5173","https://vote-app-sepia.vercel.app"], // Allow localhost & deployed frontend
+  origin: ["http://localhost:5173",], // Allow localhost & deployed frontend
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
@@ -55,7 +55,7 @@ app.use('/api/users', userRoutes);  // For user-related routes
 app.use('/api/votes', voteRoutes);  // For vote-related routes
 app.use('/', voteRoutes);  // For vote-related routes
 app.use('/api', candidateRoutes); // For candidate-related routes
-app.use(candidateRoutes);
+// app.use(candidateRoutes);
 
 console.log(process.env.MONGO_URI)
 
--- vote-app/vite.config.js
+++ vote-app/vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
-    host: true,  // Allow external access in production
     port: 5173,
   },
   build: {
-    outDir: 'dist', // Ensure the output folder is correct
-    sourcemap: true, // Helps debugging in production
-  },
-})
+    outDir: 'dist'
+  }
+});
