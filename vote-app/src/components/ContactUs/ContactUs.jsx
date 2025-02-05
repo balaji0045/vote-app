755ca5af536c13f5fae76c915f3b082dda60fa57
balaji0045 <balajidk100@gmail.com>
2025-02-05 18:59:20 +0530

    contact us page work done

--- vote-app/src/components/ContactUs/ContactUs.jsx
+++ vote-app/src/components/ContactUs/ContactUs.jsx
@@ -1,12 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import "./ContactUs.css";
 import { Link as RouterLink } from 'react-router-dom';
+import emailjs from "@emailjs/browser";
 
 
 const ContactUs = () => {
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    comments: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .send(
+        "service_to1wnao",
+        "template_w4qymtr",
+        formData,
+        "SieDyVrcev-0nUH13"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Your message has been sent successfully!");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    // Reset form data
+    setFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      comments: "",
+    });
+  };
+
+
+
+
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+
+
   return (
     <div className="contactus">
       <div className="header">
@@ -22,28 +73,44 @@ const ContactUs = () => {
             gap: "20px",
           }}
           component="form"
+          onSubmit={handleSubmit}
         >
           <TextField
             label="First Name"
             fullWidth
+            name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
           />
           <TextField
             label="Last Name"
             fullWidth
+            name="lastName"
+            value={formData.lastName}
+             onChange={handleChange}
           />
           <TextField
             label="Email"
             fullWidth
+            name="email"
+                  value={formData.email}
+                  onChange={handleChange}
           />
           <TextField
             label="Phone Number"
             fullWidth
+            name="phone"
+              value={formData.phone}
+             onChange={handleChange}
           />
           <TextField
             label="Comments"
             multiline
             rows={3}
             fullWidth
+            name="comments"
+                  value={formData.comments}
+                  onChange={handleChange}
           />
           <Button variant="outlined" type="submit">
             Submit
